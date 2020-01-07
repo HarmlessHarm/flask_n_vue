94edac64c18a7892dbbe404c971c177637cb4ebb
Harm Manders <harmmanders@gmail.com>
2020-01-07 13:36:02 +0100

    added map route to server

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
-
+import numpy as np
 
 DEBUG = True
 
@@ -14,6 +14,14 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 @app.route('/ping', methods=['GET'])
 def ping_pong():
 	return jsonify('pong!')
+
+
+@app.route('/map', methods=['GET'])
+def heatmap():
+	x, y = np.linspace(-1,1,20).reshape(1,-1), np.linspace(-1,1,20).reshape(1,-1)
+	hm = x.T @ y
+	# test = ("\n".join([str(row) for row in hm]))
+	return jsonify(list(map(list, hm)))
 
 if __name__ == '__main__':
 	app.run()
